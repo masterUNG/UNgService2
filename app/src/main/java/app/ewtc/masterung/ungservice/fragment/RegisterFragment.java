67cc2731a6faf36283c210cb6c1a77cd999b220c
MasterUNG <phrombutr@gmail.com>
2017-10-28 15:34:52 +0700

    back on Toolbar OK

--- app/src/main/java/app/ewtc/masterung/ungservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/ewtc/masterung/ungservice/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package app.ewtc.masterung.ungservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import app.ewtc.masterung.ungservice.MainActivity;
 import app.ewtc.masterung.ungservice.R;
 
 /**
@@ -14,6 +16,33 @@ import app.ewtc.masterung.ungservice.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setTitle(getResources().getString(R.string.new_register));
+
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
